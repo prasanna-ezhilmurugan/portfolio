c27c82bce69d2bbe8089d918fe8892ac23fc2b64
prasanna-ezhilmurugan <prasanna.ezhilmurugan@gmail.com>
2025-03-29 00:03:45 +0530

    add download resume option

--- app/page.tsx
+++ app/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
             radius: "full",
             variant: "shadow",
           })}
-          href={siteConfig.links.docs}
+          href={"/Prasanna-Ezhilmurugan-Resume.pdf"}
         >
           Resume
         </Link>
