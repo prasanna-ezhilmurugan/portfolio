9a17eb0b03cc51b28b10eb0a844be85b9be734cc
prasanna-ezhilmurugan <prasanna.ezhilmurugan@gmail.com>
2025-03-29 13:43:05 +0530

    add a proto contact form

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,9 +1,13 @@
 import { title } from "@/components/primitives";
+import Contact from '@/components/contact';
 
 export default function ContactPage() {
   return (
     <div>
       <h1 className={title()}>Contact</h1>
+      <main className='flex min-h-screen flex-col items-center justify-center p-24 bg-white'>
+        <Contact />
+      </main>
     </div>
   );
 }
