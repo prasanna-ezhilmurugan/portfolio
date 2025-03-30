4fa9eb5563e6780aa997065deaa70caf28a9e20c
prasanna-ezhilmurugan <prasanna.ezhilmurugan@gmail.com>
2025-03-30 15:45:08 +0530

    add heroui card template

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,9 +1,11 @@
 import { title } from "@/components/primitives";
+import ProjectCard from "@/components/ProjectCard";
 
 export default function ProjectPage() {
   return (
     <div>
       <h1 className={title()}>Projects</h1>
+      <ProjectCard />
     </div>
   );
 }
